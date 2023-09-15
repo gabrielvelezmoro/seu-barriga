3709b4dc915150c8d063ac7f38c3917e9b0f4d60
gabriel <gabrieuvelezmoro@gmail.com>
2023-09-15 10:16:37 -0300

    test: throw to save user without passwd and if user already exists

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 
 const app = require("../../src/app");
 
+const mail = `${Date.now()}@mail.com`;
 test("Deve listar todos os usuarios", async () =>
   request(app)
     .get("/users")
@@ -10,27 +11,23 @@ test("Deve listar todos os usuarios", async () =>
       expect(res.body.length).toBeGreaterThan(0);
     }));
 
-test("Deve inserir usuario com sucesso", async () => {
-  const mail = `${Date.now()}@mail.com`;
-  return request(app)
+test("Deve inserir usuario com sucesso", async () =>
+  request(app)
     .post("/users")
     .send({ name: "Walter White", mail, passwd: "12345" })
     .then((res) => {
       expect(res.status).toBe(201);
       expect(res.body.name).toBe("Walter White");
-    });
-});
+    }));
 
-test("Não deve inserir usuário sem nome", async () => {
-  const mail = `${Date.now()}@mail.com`;
-  return request(app)
+test("Não deve inserir usuário sem nome", async () =>
+  request(app)
     .post("/users")
-    .send({ mail, passwd: "12345" })
+    .send({ mail: "walter@mail.com", passwd: "12345" })
     .then((res) => {
       expect(res.status).toBe(400);
       expect(res.body.error).toBe("Nome é um atributo obrigatório");
-    });
-});
+    }));
 
 test("Não deve inserir usuário sem e-mail", async () => {
   const result = await request(app)
@@ -40,3 +37,23 @@ test("Não deve inserir usuário sem e-mail", async () => {
   expect(result.status).toBe(400);
   expect(result.body.error).toBe("Email é um atributo obrigatório");
 });
+
+test("Não deve inserir usuário sem senha", (done) => {
+  request(app)
+    .post("/users")
+    .send({ name: "Walter White", mail: "walter@mail.com" })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe("Senha é um atributo obrigatório");
+      done();
+    });
+});
+
+test("Não deve inserir usuário com email já existente", () =>
+  request(app)
+    .post("/users")
+    .send({ name: "Walter White", mail, passwd: "12345" })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe("Já existe um usuario com esse email");
+    }));
