e4ac3bba133d876be0cb3021230c2be8273f981f
gabriel <gabrieuvelezmoro@gmail.com>
2023-09-20 11:22:51 -0300

    test: should remove account

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -64,3 +64,15 @@ test("Deve alterar uma conta", () =>
           expect(res.body.user_id).toBe(user.id);
         }),
     ));
+
+test("Deve remover uma conta", () =>
+  app
+    .db("accounts")
+    .insert({ name: "Acc To Update", user_id: user.id }, ["id"])
+    .then((acc) =>
+      request(app)
+        .delete(`${MAIN_ROUTE}/${acc[0].id}`)
+        .then((res) => {
+          expect(res.status).toBe(204);
+        }),
+    ));
