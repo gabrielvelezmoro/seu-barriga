5c3d10d51c245802a29d31392ac24e65553a074d
gabriel <gabrieuvelezmoro@gmail.com>
2023-09-27 19:36:08 -0300

    feat: hash passwd

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,15 @@
+const bcrypt = require("bcrypt");
 const ValidationError = require("../errors/ValidationError");
 
 module.exports = (app) => {
-  const findAll = (filter = {}) => app.db("users").where(filter).select();
+  const findAll = () => app.db("users").select(["id", "name", "mail"]);
+
+  const findOne = (filter = {}) => app.db("users").where(filter).first();
+
+  const getPassedHash = (passwd) => {
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(passwd, salt);
+  };
 
   const save = async (user) => {
     if (!user.name) throw new ValidationError("Nome é um atributo obrigatório");
@@ -10,13 +18,15 @@ module.exports = (app) => {
     if (!user.passwd)
       throw new ValidationError("Senha é um atributo obrigatório");
 
-    const userDb = await findAll({ mail: user.mail });
+    const userDb = await findOne({ mail: user.mail });
 
-    if (userDb && userDb.length > 0)
+    if (userDb)
       throw new ValidationError("Já existe um usuario com esse email");
 
-    return app.db("users").insert(user, "*");
+    const newUser = { ...user };
+    newUser.passwd = getPassedHash(user.passwd);
+    return app.db("users").insert(newUser, ["id", "name", "mail"]);
   };
 
-  return { findAll, save };
+  return { findAll, save, findOne };
 };
